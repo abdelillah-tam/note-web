8eb54141e3fab320609348fc9dc865a5afbf6c29
Abdelillah <atamossat@gmail.com>
2024-06-05 01:03:40 +0100

    nothing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), {
+    provide: LocationStrategy, useClass: HashLocationStrategy
+  }]
 };
